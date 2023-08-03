2f4c0171da6bfb64cc02e5de51b73121bb619f89
Kennedy Owusu <kennediowusu@gmail.com>
2023-08-03 18:56:01 +0000

    create common styles for dashboard data

---
+++ lib/constants/styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+TextStyle commonDashboardStyle(double fontSize,
+    {FontWeight fontWeight = FontWeight.bold}) {
+  return TextStyle(
+    fontSize: fontSize,
+  );
+}
